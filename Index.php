83ff388ca2f5f6d43739a0794a750ebb350f6ff9
unknown <tegar29email.com@gmail.com>
2025-10-24 15:47:52 +0700

    error

--- Index.php
+++ Index.php
@@ -1,9 +1,16 @@
 <!doctype html>
-<html>
+<htmllang en>
     <head>
-        <title>Weather app</applet></title>
+        <meta name="viewport" charset="UTF-8" content="width=device-width, initial-scale=1.1">
+        <title>Weather app</title>
+        <link rel="stylesheet" href="style.css">
     </head>
     <body>
-
+        <div class="container">
+            <section class="card">
+                <h1> Hello worlds</h1>
+                <h1> Hello worlds</h1>
+            </section>
+        </div>
     </body>
 </html>
